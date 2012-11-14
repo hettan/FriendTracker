d374b71357161629cfa82dc4a151d3fc161d44be
Anders Ydremark <MrYdremark@gmail.com>
2012-11-14 11:21:20 +0100

    Setting up onClicklistener

--- FriendTracker/src/sv/teamAwesome/friendtracker/MainActivity.java
+++ FriendTracker/src/sv/teamAwesome/friendtracker/MainActivity.java
@@ -1,14 +1,32 @@
 package sv.teamAwesome.friendtracker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		EditText username = (EditText) findViewById(R.id.usernametv);
+		EditText password = (EditText) findViewById(R.id.passwordtv);
+		Button login = (Button) findViewById(R.id.loginbtn);
+		Button register = (Button) findViewById(R.id.regbtn);
+		
+		login.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				//If something, something, then do something, something.
+				
+				
+			}
+		});
 	}
+
 
 }
