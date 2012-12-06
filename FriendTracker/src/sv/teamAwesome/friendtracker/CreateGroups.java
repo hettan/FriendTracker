6f35fa8c8a8d6930a21200e6352c64ca67cb8724
Anders Ydremark <MrYdremark@gmail.com>
2012-12-06 15:42:21 +0100

    Anders har jobbat!

---
+++ FriendTracker/src/sv/teamAwesome/friendtracker/CreateGroups.java
@@ -0,0 +1,67 @@
+package sv.teamAwesome.friendtracker;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class CreateGroups extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.create_groups);
+		final Object me = this;
+		
+		Button createBtn = (Button) findViewById(R.id.createBtn);
+		final EditText createTxt = (EditText) findViewById(R.id.createTxt);
+		
+		createBtn.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				String text = createTxt.getText().toString();
+				
+				JSONObject toServer = new JSONObject();
+				JSONObject data = new JSONObject();
+				try {
+					data.put("username", Config.USERNAME);
+					data.put("name", text);
+					toServer.put("type", "createGroup");
+					toServer.put("data", data);
+				} catch (Exception e) {
+
+				}
+
+				String toSend = toServer.toString();
+
+				try {
+		            Class[] params = {String.class, Boolean.class};
+					
+					ConnectionData connData = new ConnectionData(CreateGroups.class.getMethod("Callback", params), me, toSend);
+					AsyncTask<ConnectionData, Integer, String> conn = new ConnectionHandler().execute(connData);
+				}
+				catch(Exception e) {
+
+				}
+			}
+		});
+		
+	}
+	public void Callback(String res, Boolean error) {
+		if(!error) {
+
+		} else {
+
+		}
+	}
+
+}
