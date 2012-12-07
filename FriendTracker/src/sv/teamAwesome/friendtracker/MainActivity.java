e0c8b65665ee0367fdc3459614af455b79b05142
mennon <mennon@PowerStation>
2012-12-07 14:54:27 +0100

    Notifications xml

--- FriendTracker/src/sv/teamAwesome/friendtracker/MainActivity.java
+++ FriendTracker/src/sv/teamAwesome/friendtracker/MainActivity.java
@@ -92,6 +92,7 @@ public class MainActivity extends Activity {
 				startActivity(regis);
 			}
 		});
+		
 		override.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
